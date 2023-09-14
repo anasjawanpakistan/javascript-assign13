14374720a53ce58a6ba9f23cdffdafa357995dd8
anasjawanpakistan <anasm0213@gmail.com>
2023-09-14 19:35:11 +0500

    add

---
+++ app.js
@@ -0,0 +1,96 @@
+// Q10
+// var std = [320,230,480,120];
+
+// std.sort(also work)
+
+
+// console.log(aorder)
+
+
+
+
+// Q11
+// var cityNames = ["Karachi","Lahore","Islamabad","Multan","Peshawar"];
+
+// var name = cityNames.slice(1,4);
+
+// console.log(name)
+// document.write(name)
+
+
+// Q8
+
+// var students =["faraz","anas","hamza"]
+// students.pop();for delete from end
+// students.push();for Add from end
+// students.shift();for delete from start
+// students.unshift();for add from start
+
+
+// var arr = [];
+// arr[0] = prompt("Enter first name");
+// arr [1] = prompt("Enter Middle name");
+// arr [2] = prompt("Enter Last name");
+
+// console.log(arr)
+
+// var arrName = ["faraz","anas","hamza"];
+// var studentMark = [320,230,480]
+
+// var percentage = studentMark[0] / 500*100;
+// var percentage1 = studentMark[1] / 500*100;
+// var percentage2 = studentMark[2] / 500*100;
+
+// console.log(percentage)
+// var score1 ="Score of" + " "+ arrName[0]+ " "+ "is" + " "+ studentMarks[0]+" ."+"Percentage is"+ " "+ percentage +"%"
+
+// console.log(percentage1)
+// var score2 ="Score of" + " "+ arrName[1]+ " "+ "is" + " "+ studentMarks[1]+" ."+"Percentage is"+ " "+ percentage +"%"
+
+// console.log(percentage2)
+// var score3 ="Score of" + " "+ arrName[2]+ " "+ "is" + " "+ studentMarks[2]+" ."+"Percentage is"+ " "+ percentage +"%"
+
+// document.write(score1)
+// document.write(score1)
+// document.write(score2)
+
+
+
+
+
+
+// Q6
+// var mixedArray = [1,"Bob","Now is",true];
+
+// mixedArray[0]
+// mixedArray[1]
+
+// console.log(mixedArray)
+// document.write()
+
+
+
+// Q3
+// var StringsArray = ["True","False","Anas","Faraz"]
+
+// StringsArray[1]
+// StringsArray[2]
+
+// console.log(StringsArray)
+// document.write()
+
+
+// Q4
+// var numbersarray = [1,2,3,4,5,6]
+// numbersarray[1]
+// numbersarray[2]
+// numbersarray[3]
+// numbersarray[4]
+// numbersarray[5]
+// numbersarray[6]
+
+// console.log(numbersarray)
+// document.write()
+
+
+
